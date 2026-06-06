f89490eb19f48f913eb0e266ab5241e0373144c8
Sameer Thakur <sameer@example.com>
2026-06-07 01:23:43 +0530

    chore: clean up backend debug files and tmp folder

--- prisma_check.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const output = execSync('npx prisma -v', { encoding: 'utf8' });
-  console.log(output);
-} catch (error) {
-  console.error(error.message);
-  console.error(error.stdout);
-  console.error(error.stderr);
-}
--- view_contacts.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const contacts = await prisma.contact.findMany({
-    select: { id: true, phone: true, firstName: true }
-  });
-  console.log(contacts);
-}
-main().finally(() => prisma.$disconnect());
